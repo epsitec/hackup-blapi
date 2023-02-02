ac50ea972f414155ffb958bbd6bec28d20e9452d
Pierre Arnaud <arnaud@epsitec.ch>
2023-02-02 16:35:24 +0100

    Initial commit

---
+++ Client/BallState.cs
@@ -0,0 +1,7 @@
+﻿using Blapi.Client;
+
+public sealed record BallState(double X, double Y, double Direction, double Speed)
+{
+    public double Angle => this.Direction * Math.PI / 180;
+    public double Radius => 40.0;
+}
---
+++ Client/BlapiState.cs
@@ -0,0 +1,11 @@
+﻿using Blapi.Client;
+
+public sealed record BlapiState(double Distance, int Index)
+{
+    public int GetSprite() => (this.Index % 4) switch
+    {
+        1 => 1,
+        3 => 2,
+        _ => 0
+    };
+}
